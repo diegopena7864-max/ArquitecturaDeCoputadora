ad7b93ef30d6d0ad989b999ec383b76fc009b62d
diegopena7864-max <diego.pena7864@alumnos.udg.mx>
2025-11-30 17:04:50 -0600

    Actividad 1
    primera actividad en clase

---
+++ _and.v
@@ -0,0 +1,10 @@
+//Definicion del modulo y susu I/0
+//dentro del parentesis se define los I/O
+
+module _and(input a, input b, output c);
+//2.definicion o compuertas internas
+//NA
+//3.Asignaciones, Intancias, Conexiones
+
+assign C = a & b;
+endmodule
